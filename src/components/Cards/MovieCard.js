1ee51815e70d49e7c928035bae2e16e0eb15f020
rakeshkokkula <rakeshkokkula3@gmail.com>
2022-11-06 15:52:55 +0530

    changes

--- src/components/Cards/MovieCard.js
+++ src/components/Cards/MovieCard.js
@@ -5,7 +5,7 @@ import Link from "next/link"
 const MovieCard = ({ data }) => {
   return (
     <Link href={`movie/${data.id}`}>
-      <div className=" cursor-pointer relative m-2">
+      <div className="cursor-pointer relative m-2">
         <Image
           //   src={`https://images.unsplash.com/photo-1667566286356-48462d0123b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80`}
           src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
