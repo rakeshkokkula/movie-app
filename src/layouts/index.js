526ac281b374bd157bb94df624f93cca7460958f
rakeshkokkula <rakeshkokkula3@gmail.com>
2022-11-06 16:52:21 +0530

    css change

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import Footer from "../components/Footer/Footer"
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-column min-h-screen">
       <Navbar />
       <main className="flex-grow">{children}</main>
       <Footer />
